42b36fe851d943e35f68b9d60500c5977244d6b6
Bill Gestrich <bill@foreflight.com>
2021-11-06 07:52:56 -0400

    Use unique

--- pi-daemon.sh
+++ pi-daemon.sh
@@ -39,7 +39,7 @@ while true; do
   rtl_tcp_pid=$! # Save the pid for murder later
   sleep 10 #Let rtl_tcp startup and open a port
 
-  json="$(sudo /home/pi/go/bin/rtlamr -msgtype=r900 -filterid=$METERID -single=true -format=json)"
+  json="$(sudo /Users/bill/go/bin/rtlamr -msgtype=r900 -filterid=$METERID -single=true -format=json -unique=true)"
 
   echo "Meter info: $json"
 
@@ -48,10 +48,11 @@ while true; do
     consumption="$(echo "$json" | jq .Message.Consumption)"
     echo  "$(generate_post_data $timeStamp $consumption)"
     #In your bashrc, put something like export WATER_MONITOR_URL=<URL to your lambda service>
+    WATER_MONITOR_URL="https://eo2gdd7nhj.execute-api.us-east-1.amazonaws.com/Prod/waterReading"
     curl -X POST -H "Content-Type: application/json" "${WATER_MONITOR_URL}" \
     --data "$(generate_post_data $timeStamp $consumption)"
 
-  kill $rtl_tcp_pid # rtl_tcp has a memory leak and hangs after frequent use, restarts required - https://github.com/bemasher/rtlamr/issues/49
+  kill $rtl_tcp_pid || true # rtl_tcp has a memory leak and hangs after frequent use, restarts required - https://github.com/bemasher/rtlamr/issues/49
 
   #Going to try without the sleep - This may be required for the tcp call to work though
   #sleep 60
